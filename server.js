ae65f6630bed8f56478f2a84f59885f2e2851abd
Orkhan Mirzoev <166619603+QTDevi@users.noreply.github.com>
2025-01-26 17:23:30 +0400

    Update server.js
    Updated server.js to test CI/CD

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, CI/CD!");
+    res.send("Hello, CI/CD! Pipeline is working?");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
